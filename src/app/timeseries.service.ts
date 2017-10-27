9b46d8f836cca2d591fcf4508f481f0e393d2438
Chris Tapper <cj.tapper@gmail.com>
2017-10-27 11:55:44 +1100

    Fix bug where metadata wouldn't be loaded until the first DAP request was made. Refactor for ease of reading.

--- src/app/timeseries.service.ts
+++ src/app/timeseries.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Http } from '@angular/http';
-import { ProjectionService } from "map-wald/index";
-import { GeoTransform } from "app/charts/geotransform";
-import { LatLng } from "app/latlng";
+import { ProjectionService } from 'map-wald/index';
+import { GeoTransform } from 'app/charts/geotransform';
+import { LatLng } from 'app/latlng';
 
 import 'rxjs/add/observable/from';
 import 'rxjs/add/operator/publishReplay';
@@ -13,19 +13,20 @@ import 'rxjs/add/operator/toArray';
 import 'rxjs/add/operator/switch';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/mergeMap';
 
-let dap = require('dap-query-js');
+const dap = require('dap-query-js');
 
-const DAP_SERVER='http://dapds00.nci.org.au/thredds/dodsC/ub8/au/FMC/';
+const DAP_SERVER = 'http://dapds00.nci.org.au/thredds/dodsC/ub8/au/FMC/';
 
-export interface FmcTile{
-  dataSet:string;
-  filename:string;
-  year:number;
-  tile:string;
+export interface FmcTile {
+  dataSet: string;
+  filename: string;
+  year: number;
+  tile: string;
 }
 
-export interface TileCell{
+export interface TileCell {
   tile: string;
   cell: Array<number>;
 }
@@ -33,162 +34,192 @@ export interface TileCell{
 
 @Injectable()
 export class TimeseriesService {
-  projection:any;
-  dasCache:{[key:string]:any}={};
-  ddxCache:{[key:string]:any}={};
-  geoTransforms:{[key:string]:GeoTransform}={};
-  files$:Observable<Array<FmcTile>>;
-  das$:Observable<Array<[FmcTile,any]>>;
-  ddx$:Observable<Array<[FmcTile,any]>>;
-  geoTransforms$:Observable<Array<[string,GeoTransform]>>;
+  projection: any;
+  dasCache: {[key: string]: any} = {};
+  ddxCache: {[key: string]: any} = {};
+  geoTransforms: {[key: string]: GeoTransform} = {};
+  files: Observable<FmcTile[]>;
+  das$: Observable<Array<[FmcTile, any]>>;
+  ddx$: Observable<Array<[FmcTile, any]>>;
+  geoTransforms$: Observable<Array<[string, GeoTransform]>>;
 
-  getAllMetadata(paths$:Observable<FmcTile[]>,metaType:string,parser:(string)=>any){
-    return (paths$.map(entries=>{
-      return Observable.from(entries.map(tile=>{
-        return <Observable<[FmcTile,any]>>this.http.get(`${DAP_SERVER}${tile.filename}.${metaType}`)
-          .map(resp=>resp.text())
-          .map(parser)
-          .map(metadata=>[tile,metadata]);
-      })).mergeAll();
-    }))
-      .switch()
-      .toArray()
-      .publishReplay()
-      .refCount();
+  getMetaData(tile: FmcTile, metaType: string, parser: (string) => any): Observable<[FmcTile, string]> {
+
+    return this.http.get(`${DAP_SERVER}${tile.filename}.${metaType}`)
+                                .map(resp => resp.text())
+                                .map(parser)
+                                .map(metadata => [tile, metadata]);
   }
 
-  constructor(private http:Http, ps:ProjectionService) {
-    var proj4 = ps.proj4();
-    var def ="+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs";
+
+  getAllMetadata(metaType: string, parser: (string) => any): Observable<[FmcTile, string][]> {
+
+    return this.files.flatMap(files => {
+
+      return Observable.from(files.map(tile => { return this.getMetaData(tile, metaType, parser); }))
+        .mergeAll()
+        .toArray();
+
+    });
+
+  }
+
+  private parseFileList(fileObject: any): Array<FmcTile> {
+
+    const fileList = fileObject.files;
+    return fileList.map(fullFn => {
+
+      const [dir, fn] = fullFn.split('/');
+
+      let elements: Array<string>;
+      if (dir === 'sinusoidal') {
+        elements = fn.split('.');
+      } else {
+        elements = fn.split('_');
+        elements[2] = elements[2].split('.')[0];
+      }
+
+      return {
+        filename: fullFn,
+        dataSet: dir,
+        year: +elements[1],
+        tile: elements[2]
+      };
+    });
+
+  }
+
+  constructor(private http: Http, ps: ProjectionService) {
+    const proj4 = ps.proj4();
+    const def = '+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs';
 
     this.projection = proj4(def);
 
-    this.files$ = http.get('assets/config/fmc_filelist.json').map(r=>r.json()).map(val=>{
-      var fileList = val.files;
-      return fileList.map(fullFn=>{
-        var [dir,fn] = fullFn.split('/');
-        var elements:Array<string>;
-        if(dir==='sinusoidal'){
-          elements = fn.split('.');
-        } else {
-          elements = fn.split('_')
-          elements[2] = elements[2].split('.')[0];
-        }
+    this.files = http.get('assets/config/fmc_filelist.json')
+      .map(r => r.json())
+      .map(this.parseFileList)
+      .publishReplay(1)
+      .refCount();
 
-        return {
-          filename:fullFn,
-          dataSet:dir,
-          year:+elements[1],
-          tile:elements[2]
-        };  
-      })
-    }).publishReplay().refCount();
+    this.das$ = this.files
+      .flatMap(files => this.getAllMetadata('das', dap.parseDAS))
+      .publishReplay(1)
+      .refCount();
 
-    var uniqueTiles$ = this.files$.map(allFiles=>{
-      return Array.from(new Set(allFiles.map(t=>`${t.tile}-${t.dataSet}`)));
-    });
+    this.ddx$ = this.files
+      .flatMap(files => this.getAllMetadata('ddx', dap.parseDDX))
+      .publishReplay(1)
+      .refCount();
 
-    var pathsToTiles$ = Observable.forkJoin(this.files$,uniqueTiles$).map(([files,uniqueTiles])=>{
-      return uniqueTiles.map(tileIDandDataset=>{
-        var [tileID, dataSet] = tileIDandDataset.split('-');
-        var tile = files.find(t=>t.tile===tileID);
-        return [tileID,dataSet,tile.filename];
+    // Subscribing to these observables now forces them to resolve. Otherwise
+    // they are lazy, and won't resolve until they are subscribed to later on.
+    this.files.subscribe(() => console.log('FMC Filelist retrieved'));
+    this.das$.subscribe(() => console.log('das metadata retrieved'));
+    this.ddx$.subscribe(() => console.log('ddx metadata retrieved'));
+
+    this.geoTransforms$ = this.das$.map(allDAS => {
+      const uniqueTiles = Array.from(new Set(allDAS.map(([t, das]) => t.tile)));
+
+      const dasForUniqueTiles = uniqueTiles.map(tileID => {
+        return allDAS.find(([t, das]) => t.tile === tileID);
       });
-    });
 
-    this.das$ = this.getAllMetadata(this.files$,'das',dap.parseDAS);
-
-    this.geoTransforms$ = this.das$.map(allDAS=>{
-//      var justSinusoidal = allDAS.filter(([tile,das])=>tile.dataSet==='sinusoidal');
-      var uniqueTiles = Array.from(new Set(allDAS.map(([t,das])=>t.tile)));
-      var dasForUniqueTiles = uniqueTiles.map(tileID=>{
-        return allDAS.find(([t,das])=>t.tile===tileID);
-      })
-      return dasForUniqueTiles.map((x)=>{
-        var [tile,das] = x;
-        var tmp = das.variables.sinusoidal;
-        var geo = tmp.GeoTransform.trim().split(' ').map(s=>+s);
-        return [tile.tile,new GeoTransform(geo)];
+      return dasForUniqueTiles.map((x) => {
+        const [tile, das] = x;
+        const tmp = das.variables.sinusoidal;
+        const geo = tmp.GeoTransform.trim().split(' ').map(s => +s);
+        return [tile.tile, new GeoTransform(geo)];
       });
     }).publishReplay().refCount();
 
-    // var pathToAllTiles$ = this.files$.map(files=>{
-    //   return files.map(file=>{
-    //     return [
-    //       file.tile,
-    //       file.dataSet,
-    //       file.filename
-    //     ];
-    //   });
-    // });
-    this.ddx$ = this.getAllMetadata(this.files$,'ddx',dap.parseDDX);
   }
 
-  findTile(ll:LatLng):Observable<TileCell>{
-    var projected = this.projection.forward([ll.lng,ll.lat]);
+  findTile(ll: LatLng): Observable<TileCell> {
+    const projected = this.projection.forward([ll.lng, ll.lat]);
 
-    return Observable.forkJoin(this.das$,this.geoTransforms$,this.ddx$)
-      .map(([allDAS,allGeo,allDDX])=>{
-        return Observable.from(allGeo.map(([tileID,geotransform],i)=>{
-          var [row,col] = geotransform.toRowColumn(projected[0],projected[1]);
-          col -= 0.5;
-          var ddx = this._match({tile:tileID},allDDX);
+    return Observable.forkJoin(this.das$, this.geoTransforms$, this.ddx$)
+    .map(([allDAS, allGeo, allDDX]) => {
+      return Observable.from(allGeo.map(([tileID, geotransform], i) => {
+        let [row, col] = geotransform.toRowColumn(projected[0], projected[1]);
+        col -= 0.5; // FIXME: Insert comment - why are we making this adjustment?
+        const ddx = this._match({tile: tileID}, allDDX);
 
-          if((row<0)||(Math.floor(row)>=+ddx.variables.x.dimensions[0].size)||
-            (col<0)||(Math.floor(col)>=+ddx.variables.y.dimensions[0].size)){
+        if ((row < 0) || (Math.floor(row) >= +ddx.variables.x.dimensions[0].size) ||
+          (col < 0) || (Math.floor(col) >= +ddx.variables.y.dimensions[0].size)) {
+          return null;
+        }
+
+        //          console.log('MATCH: '+tileID,row,col);
+        return {
+          tile: tileID,
+          cell: [Math.floor(row), Math.floor(col)]
+        };
+      }));
+    }).switch().first(tc => tc !== null);
+  }
+/*
+  findTile(ll: LatLng): Observable<TileCell> {
+    const projected = this.projection.forward([ll.lng, ll.lat]);
+
+    return Observable.forkJoin(this.das$, this.geoTransforms$, this.ddx$)
+      .map(([allDAS, allGeo, allDDX]) => {
+        return Observable.from(allGeo.map(([tileID, geotransform], i) => {
+          let [row, col] = geotransform.toRowColumn(projected[0], projected[1]);
+          col -= 0.5; // FIXME: Insert comment - why are we making this adjustment?
+          const ddx = this._match({tile: tileID}, allDDX);
+
+          // Check if bounds are within this tile
+          if ((row < 0) || (Math.floor(row) >= +ddx.variables.x.dimensions[0].size) ||
+            (col < 0) || (Math.floor(col) >= +ddx.variables.y.dimensions[0].size)) {
             return null;
           }
 
-//          console.log('MATCH: '+tileID,row,col);
           return {
-            tile:tileID,
-            cell:[Math.floor(row),Math.floor(col)]
+            tile: tileID,
+            cell: [Math.floor(row), Math.floor(col)]
           };
         }));
-      }).switch().first(tc=>tc!==null);
-  }
+      }).switch().first(tc => tc! == null);
+  }*/
 
-  _match(search:any,pairs:Array<[FmcTile,any]>):any{
-    var result = pairs.find(([t,o])=>{
-      for(var k in search){
-        if(search[k]!==t[k]){
+  _match(search: any, pairs: Array<[FmcTile, any]>): any {
+    const result = pairs.find(([t, o]) => {
+      for (const k in search) {
+        if (search[k] !== t[k]) {
           return false;
         }
       }
       return true;
     });
-    
-    if(result){
+
+    if (result) {
       return result[1];
     }
     return undefined;
   }
 
-  getTimeSeries(point:LatLng,year:number):Observable<any>{
-    return Observable.forkJoin(this.findTile(point),this.files$,this.das$,this.ddx$)
-      .map(([tileMatch,files,allDAS,allDDX])=>{
-        var tile = tileMatch.tile;
-        var cell = tileMatch.cell;
-        var matchCriteria = {year:year,tile:tile};
-        var das = this._match(matchCriteria,allDAS);
-        var ddx = this._match(matchCriteria,allDDX);
-        var tileFile = files.find(f=>(f.tile===tile)&&(f.year===year));
+  getTimeSeries(point: LatLng, year: number): Observable<any> {
 
-        if(!tileFile){
+    return Observable.forkJoin(this.findTile(point), this.files, this.das$, this.ddx$)
+      .map(([tileMatch, files, allDAS, allDDX]) => {
+        const tile = tileMatch.tile;
+        const cell = tileMatch.cell;
+        const matchCriteria = {year: year, tile: tile};
+        const das = this._match(matchCriteria, allDAS);
+        const ddx = this._match(matchCriteria, allDDX);
+        const tileFile = files.find(f => (f.tile === tile) && (f.year === year));
+
+        if (!tileFile) {
           return Observable.throw(new Error(`No time series data at ${tile} in ${year}`));
         }
-        // console.log(das);
-        // console.log(ddx);
-        var nTimeSteps = +(ddx.variables.time.dimensions[0].size);
-        //console.log(ddx.variables.time.dimensions,nTimeSteps);
-        var filename = tileFile.filename;
-        var [r,c] = cell;
-        var variable='lvmc_mean';
-        //variable =  filename.split('/')[1].split('_')[0].toLowerCase() + '_mean';
-        var url = `${DAP_SERVER}${filename}.ascii?${variable}[0:1:${nTimeSteps-1}][${r}:1:${r}][${c}:1:${c}]`;
-        return this.http.get(url).map(r=>r.text())
-          .map(txt=>dap.parseData(txt,das))
-          .map(dap.simplify)
+        const nTimeSteps = +(ddx.variables.time.dimensions[0].size);
+        const filename = tileFile.filename;
+        const [r, c] = cell;
+        const variable = 'lvmc_mean';
+        const  url = `${DAP_SERVER}${filename}.ascii?${variable}[0:1:${nTimeSteps - 1}][${r}:1:${r}][${c}:1:${c}]`;
+        return this.http.get(url).map(res => res.text())
+          .map(txt => dap.parseData(txt, das))
+          .map(dap.simplify);
       }).switch();
   }
 }
