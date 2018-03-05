84f9d76297abebb9873d537b7e87ed517cab0a5a
Chris Tapper <chris.tapper@anu.edu.au>
2018-03-05 12:03:16 +1100

    Refactor VisibleLayer into it's own file for better seperation of concerns

--- src/app/main-map/main-map.component.ts
+++ src/app/main-map/main-map.component.ts
@@ -1,106 +1,28 @@
-import {Component, ViewChild, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Http} from '@angular/http';
-import {
-  WMSService, WMSLayerComponent, MapViewParameterService,
-  InterpolationService, TimeseriesService, CatalogHost
-} from 'map-wald';
+import {CatalogHost, MapViewParameterService, TimeseriesService, WMSLayerComponent, WMSService} from 'map-wald';
 import {SelectionService} from '../selection.service';
 import {VectorLayer} from '../vector-layer-selection/vector-layer-selection.component';
 import {LatLng} from '../latlng';
 import {BaseLayer} from '../base-layer.service';
 import {LayersService} from '../layers.service';
 import {environment} from '../../environments/environment';
-import {FMCLayer, DateRange} from '../layer';
+import {DateRange, FMCLayer} from '../layer';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/observable/forkJoin';
+import {VisibleLayer} from './visible-layer';
+
 const TDS_URL = environment.tds_server;
 
 class ValueMarker {
   loc: LatLng;
   value: string;
   open: boolean;
-}
-
-export class VisibleLayer {
-  url: string = TDS_URL;
-  path: string;
-  legendImageURL: string = null;
-  opacity = 1.0;
-  wmsParameters: any;
-
-  static leading0(n: number): string {
-    if (n < 10) {
-      return '0' + n;
-    }
-    return '' + n;
-  }
-
-  applyFixed() {
-    if (this.layer.wmsParams) {
-      Object.assign(this.wmsParameters, this.layer.wmsParams);
-    }
-  }
-
-  dateText(date: Date): string {
-    const fmt = this.layer.timePeriod.format || '{{year}}-{{month}}-{{day}}T00%3A00%3A00.000Z';
-    return InterpolationService.interpolate(fmt, {
-      year: date.getFullYear(),
-      month: VisibleLayer.leading0(date.getMonth() + 1),
-      day: VisibleLayer.leading0(date.getDate())
-    });
-  }
-
-  setDate(newDate: Date) {
-    this.updateParameters(newDate);
-  }
-
-  constructor(public layer: FMCLayer, currentDate: Date) {
-    if (layer) {
-      this.updateParameters(currentDate);
-    }
-  };
-
-  updateParameters(currentDate: Date) {
-    this.path = InterpolationService.interpolate(this.layer.path, {
-      year: currentDate.getFullYear(),
-      month: VisibleLayer.leading0(currentDate.getMonth() + 1),
-      day: VisibleLayer.leading0(currentDate.getDate())
-    });
-
-    if (this.layer.source === 'tds') {
-      this.url = `${this.layer.host || TDS_URL}/wms/${this.path}`;
-    }
-
-    this.wmsParameters = {
-      layers: this.layer.variable,
-      time: this.dateText(currentDate),
-      styles: '',
-      transparent: true,
-      tiled: true,
-      feature_count: 101
-    };
-
-    if (currentDate > this.layer.timePeriod.end) {
-      this.wmsParameters.time = this.dateText(this.layer.timePeriod.end);
-    }
-
-    this.applyFixed();
-
-    if (this.layer.palette && this.layer.palette.image) {
-      this.legendImageURL = this.getLegendUrl();
-    }
-  }
-
-  getLegendUrl(): string {
-    const p = this.wmsParameters;
-    return `${this.url}?request=GetLegendGraphic&layer=${p.layers}&palette=${p.styles.split('/')[1]}\
-        &colorscalerange=${this.layer.range.join(',')}&numcolorbands=${p.numcolorbands}`;
-  }
 }
 
 @Component({
@@ -169,10 +91,10 @@ export class MainMapComponent implements OnInit {
 
     this.mainLayer = new VisibleLayer(null, null);
 
-    this.layers.availableLayers.subscribe(available => {
+    this.layers.availableLayers.subscribe(() => {
       this.selection.loadFromURL(_activatedRoute);
       this.selection.dateChange.subscribe((newDate: Date) => {
-        this.dateChanged(newDate);
+        this.setDate(newDate);
       });
     });
 
@@ -264,7 +186,7 @@ export class MainMapComponent implements OnInit {
       return;
     }
 
-    this.timeseries.getTimeseries(this.layerHost, fn, this.mainLayer.layer.variable, coords, this.mainLayer.layer.indexing)// ,year)
+    this.timeseries.getTimeseries(this.layerHost, fn, this.mainLayer.layer.variable, coords)// ,year)
       .subscribe(dapData => {
           if ((year !== this.selection.year) || (coords !== this.marker.loc)) {
             return; // Reject the data
@@ -293,12 +215,8 @@ export class MainMapComponent implements OnInit {
   }
 
 
-
-  dateChanged(newDate: Date) {
+  setDate(newDate: Date) {
     this.mainLayer.setDate(newDate);
-  }
-
-  ngAfterViewInit() {
   }
 
   ngOnInit() {
@@ -325,11 +243,11 @@ export class MainMapComponent implements OnInit {
       });
   }
 
-  baseLayerChanged(layer: BaseLayer) {
+  setBaseLayer(layer: BaseLayer) {
     this.baseLayer = layer;
   }
 
-  opacityChanged(opacity: number) {
+  setOpacity(opacity: number) {
     this.mainLayer.opacity = opacity;
   }
 
