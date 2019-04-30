28eb480ba864dea89db0b10a87a231e96071e63c
Joel Rahman <joel@flowmatters.com.au>
2019-04-30 22:04:37 +1000

    Added flammability deciles as a layer

--- src/app/layer.ts
+++ src/app/layer.ts
@@ -4,14 +4,13 @@ const MAXIMUM_DATE_SHIFT=60;
 
 export class FMCLayer {
   colours: Array<string>;
-  labels: Array<string>;
 
   constructor(public name: string, public units: string, public icon: string, public variable_name: string, public palette: any,
     public range: Array<number>, public description: string, public timePeriod: DateRange,
     public legend: Array<any>, public wmsParams: any, public source: string, public path: string,
     public chartConfig: any, public host: string, public urlFragment: string, public indexing: any,
     public suffix: string, public timeshift: number, public timestep: number, public precision: number,
-    public refDate: string,public op:string, public window:number) {
+    public refDate: string,public op:string, public window:number, public labels:string[]) {
 
     this.indexing = this.indexing || {};
     // if (chartConfig) {
