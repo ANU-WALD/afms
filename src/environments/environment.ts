59fe38ea2bd35f5775095ee9c26c5741c5f82107
Joel Rahman <joel@flowmatters.com.au>
2020-05-15 11:14:20 +1000

    Fix THREDDS url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  tds_server: 'http://proxies.hydrograph.io/nci-cors/thredds',
+  tds_server: 'http://dapds00.nci.org.au/thredds',
   gsky_server: 'http://130.56.242.4/ows',
   production: false,
   google_maps_api_key: 'WENFO_GOOGLE_MAPS_API_KEY'
